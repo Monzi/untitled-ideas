565034ebd1178d18652d3e2ebdacdad9631f67f2
ckrestion <krestion@outlook.dk>
2025-06-28 16:14:35 +0200

    REWORK RC

---
+++ common/defines/00_uisdefines.lua
@@ -0,0 +1,8 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NEconomy.TRADE_PROPAGATE_DIVIDER = 30
+NDefines.NEconomy.TRADE_PROPAGATE_DIVIDER = 50
+NDefines.NEconomy.CARAVAN_POWER_MAX = 20
+NDefines.NEconomy.CARAVAN_POWER_MIN = 1
+NDefines.NEconomy.MERCHANT_MAX_POWER_BONUS = 0.5
+NDefines.NEconomy.CARAVAN_FACTOR = 15
